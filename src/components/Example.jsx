ca84e602fe090fbee5d3622bde4df5ece52a8758
ultra217 <test@test.com>
2022-09-07 21:11:51 -0400

    add

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Example() {
   return (
-    <div>了凡四訓</div>
+    <div>
+      <h1>了凡四訓</h1>
+      <div>但行好事 莫問前程</div>
+      <div>可以閱讀這本書 看清自己的人生</div>
+    </div>
   )
 }
